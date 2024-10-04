0f16db5012b5b54a32cf3ed8066a1a84be8e2a25
Ryan Serra <ryan16serrasilva@gmail.com>
2024-10-04 14:16:32 -0300

    Refatorando as jogadas do robo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mod[0].addEventListener('click', function(){
   mod_game.style.display = "none";
   container.style.display = "grid";
 
-  if (count === 1) {
+  if (count == 1) {
       for (let i = 0; i < opcao.length; i++) {
           opcao[i].style.display = 'block';
           opcao[i].style.textAlign = 'center';
@@ -36,114 +36,19 @@ mod[0].addEventListener('click', function(){
 });
 
 function robo_jogada() {
-    if ((opcao[0].textContent === x || opcao[2].textContent === x || opcao[6].textContent === x || opcao[8].textContent === x ||(opcao[1].textContent==x)||(opcao[7].textContent==x)) &&(opcao[4].textContent == '')) {
-        escolha[4].innerText = o;
-        opcao[4].textContent = `${escolha[4].textContent}`
-  } else if (opcao[4].textContent === x && opcao[0].textContent=='') {
-      escolha[4].innerText = o;
-      opcao[0].innerText = `${escolha[4].textContent}`;
-  }
- 
-  else if ((opcao[1].textContent === x || opcao[3].textContent === x || opcao[5].textContent === x || opcao[7].textContent === x) &&opcao[4].textContent === '' ){
-        opcao[0].textContent=o
-  }
-  
-  else if ((opcao[1].textContent == x && (opcao[6].textContent == x || opcao[8].textContent == x))||(opcao[7].textContent == x && (opcao[0].textContent == x || opcao[2].textContent == x)) && opcao[3].textContent=='') {
-    opcao[3].textContent = o
-}
-
-else if ((opcao[1].textContent==x && (opcao[6].textContent==x && opcao[5].textContent==x))||(opcao[7].textContent==x && (opcao[0].textContent==x && opcao[5].textContent==x))&& opcao[2].textContent==''){
-    opcao[2].textContent=o
-}
-  else{
-    for(let j=1; j<=8; j ++ ){
-  
-            i=0
-            if((j==1||j==3||j==4) && (i ==0 && opcao[j*2].textContent=='') &&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                j*=2
-                opcao[j].textContent=o
-                break;
-            }
-            if((j==2||j==6||j==8) && (i ==0 && opcao[j/2].textContent=='') &&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                j/=2
-                opcao[j].textContent=o
-                break;
-    
-            }
-            i=1
-            if((i==1 && j==4 ) && (opcao[7].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[7].textContent=o
-                break
-            }
-            if((i==1 && j==2 ) && (opcao[0].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[0].textContent=o
-                break
-            }
-            if((i==2 && j==5 ) && (opcao[8].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[7].textContent=o
-                break
-            }
-            i=2
-            if((i==2 && j==4 ) && (opcao[6].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[6].textContent=o
-                break
-            }
-            if((i==2 && j==5 ) && (opcao[8].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[8].textContent=o
-                break
-            }
-            if((i==2 && j==8 ) && (opcao[5].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[5].textContent=o
-                break
-            }
-            i=3
-            if((i==3 && j==4 ) && (opcao[5].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[5].textContent=o
-                break
-            }
-            if((i==3 && j==6 ) && (opcao[0].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[0].textContent=o
-                break
-            }
-           
-            i=6
-            if((i==6 && j==7 ) && (opcao[8].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[8].textContent=o
-                break
-            }
-            if((i==6 && j==8 ) && (opcao[7].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[7].textContent=o
-                break
-            }
-            if((i==6 && j==4 ) && (opcao[2].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[2].textContent=o
-                break
-            }
-            i=7
-            if((i==7 && j ==8) &&(opcao[6].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[6].textContent=o
-                break
-            }
-            if((i==7 && j ==1) &&(opcao[4].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[4].textContent=o
-                break
-            }
-            i=8
-            if((i==8 && j==5 ) && (opcao[2].textContent=='')&&(opcao[i].textContent==x && opcao[j].textContent==x)){
-                opcao[2].textContent=o
-                break
-            }
-            
-
-           
-
-            
-            
-        }
-
+    let randomJogada=  Math.floor(Math.random() *10);
+    if(count==0){
+    for(let i=0; i<10;i++){
+    if(opcao[randomJogada].textContent==''){
+       escolha[randomJogada].innerText = o;
+       opcao[randomJogada].textContent = `${escolha[randomJogada].textContent}`
+       break;
     }
-    Verificar_vencedor()
-
+    else randomJogada=Math.floor(Math.random()*10) 
+}
+}
+Verificar_vencedor()
+   
   count=1
   }
 
